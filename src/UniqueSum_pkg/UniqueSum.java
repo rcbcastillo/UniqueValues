cc57db578040082b143cca963565d3f65f3fb988
rcbcastillo <rcbcastillo@gmail.com>
2023-03-10 15:04:06 +0000

    Add UniqueSum source file

---
+++ src/UniqueSum_pkg/UniqueSum.java
@@ -0,0 +1,33 @@
+package UniqueSum_pkg;
+
+public class UniqueSum {
+
+	static void sumIfUniqueValues(int a, int b, int c) {
+
+		if (a != b && a != c && b != c) {
+			int sum = a + b + c;
+			System.out.println(sum);
+		} else {
+
+			if (a == b && a != c) {
+				int sum = c;
+				System.out.println(sum);
+			} else if (a != b && b == c) {
+				int sum = a;
+				System.out.println(sum);
+
+			} else if (a == b && b == c) {
+				int sum = 0;
+				System.out.println(sum);
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		sumIfUniqueValues(1, 2, 3);
+		sumIfUniqueValues(3, 3, 3);
+		sumIfUniqueValues(1, 1, 2);
+		sumIfUniqueValues(1, 2, 2);
+	}
+}
