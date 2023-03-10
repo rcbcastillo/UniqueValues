db0da6ec0cb03edda43eda2a46b8ca40fe306a17
rcbcastillo <rcbcastillo@gmail.com>
2023-03-10 16:19:07 +0000

    Add changes to UniqueSum source file

--- src/UniqueSum_pkg/UniqueSum.java
+++ src/UniqueSum_pkg/UniqueSum.java
@@ -16,6 +16,10 @@ public class UniqueSum {
 				int sum = a;
 				System.out.println(sum);
 
+			} else if (a == c) {
+				int sum = b;
+				System.out.println(sum);
+
 			} else if (a == b && b == c) {
 				int sum = 0;
 				System.out.println(sum);
@@ -29,5 +33,6 @@ public class UniqueSum {
 		sumIfUniqueValues(3, 3, 3);
 		sumIfUniqueValues(1, 1, 2);
 		sumIfUniqueValues(1, 2, 2);
+		sumIfUniqueValues(1, 2, 1);
 	}
 }
